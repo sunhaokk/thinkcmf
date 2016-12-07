58ae6faab62d23de2b6025b046da663c36710967
woainixyz1 <494020937@qq.com>
2016-12-07 15:48:19 +0800

    1

---
+++ application/Admin/Controller/CsController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AdminbaseController;
+class CsController extends AdminbaseController{
+	//库存列表
+	public function inventory(){
+		$this->display();
+	}
+}
